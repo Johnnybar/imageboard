67d2aecd914f3f34bbf9c6a5dc69cf03d9b16180
Johnnybar <johnnybareket@gmail.com>
2017-11-23 12:19:50 +0100

    image appearing when clicked

--- public/script.js
+++ public/script.js
@@ -3,14 +3,14 @@ var imageboard = angular.module('imageboard', ["app.routes"]);
 //__________DISPLAY THE IMAGES
 
 
-imageboard.controller('images', ($scope, $http, $location) => {
+imageboard.controller('images', ($scope, $http) => {
     $scope.message = 'Here are the images:';
     $http.get("/images").then(function(resp) {//This script gets the json file (containing
         //the references to the images we got from the db) that is currently being sent to
         // '/images' and adds it to the scope
 
         $scope.images = resp.data;
-        console.log(resp.data);
+
 
         $scope.title = 'Title';
         $scope.user = 'User Name';
@@ -38,14 +38,30 @@ imageboard.controller('images', ($scope, $http, $location) => {
                 transformRequest: angular.identity
             })
                 .then(() => {
-                    console.log("it worked!");
+                    setTimeout(()=>{
+                        console.log('YOOAOA');
+                        $http.get('/images')
+                            .then((resp)=>{
+                                $scope.images = resp.data;
+                            });
+                    }, 3500);
 
                 });
         };
-    }).then(()=>{
-        $http.get('/singleImg/'+ + $location.path().split('/').pop()).then((resp)=>{
-            $scope.data = resp.data;
-
-        });
     });
 });
+
+imageboard.controller('photos', ($scope, $http, $location)=>{
+    $http.get('/singleImg/'+ $location.path().split('/').pop()).then((resp)=>{
+        $scope.clickedImg = resp.data[0];
+        console.log($scope.clickedImg);
+    });
+});
+
+
+// setTimeout(()=>{
+//     $http.get('/images')
+//         .then((res)=>{
+//             $scope.images = res.data;
+//         });
+// }, 100);
