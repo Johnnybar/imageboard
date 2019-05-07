caf033014433886e163da497e980d7fbc5a1571b
johnnybar <johnnybareket@gmail.com>
2019-05-07 15:39:12 +0200

    minor changes

--- public/script.js
+++ public/script.js
@@ -5,23 +5,23 @@ location.hash = '/home';
 
 imageboard.controller('images', ($scope, $http) => {
 
-  
+
   getImages();
   $scope.message = 'Here are the images:';
 
   function getImages() {
-    $http.get("/images").then(function(resp) {
+    $http.get("/images").then(function (resp) {
       // console.log(JSON.parse(resp.data));
-      
-        $scope.images = JSON.parse(resp.data).results
-        $scope.seeds = JSON.parse(resp.data).info
-        // $scope.imageSeeds= JSON.parse(resp.data).info
+
+      $scope.images = JSON.parse(resp.data).results
+      $scope.seeds = JSON.parse(resp.data).info
+      // $scope.imageSeeds= JSON.parse(resp.data).info
       $scope.limit = 20;
       // let str = $scope.images
       // let obj = JSON.parse(str)
       // console.log('in getimages in script', JSON.parse(resp.data));
-      
-      $scope.limitUp = function() {
+
+      $scope.limitUp = function () {
         $scope.limit += 10;
       };
       $scope.title = '';
@@ -31,15 +31,15 @@ imageboard.controller('images', ($scope, $http) => {
     });
   }
 
-  $scope.submit = function() {
+  $scope.submit = function () {
     alert('Pic submit disabled in demo mode', 'ok');
   };
 });
 
 imageboard.controller('photos', ($scope, $http, $location) => {
   let str = $location.path().split('/').pop()
-  
-  
+
+
   $http.get('/singleImg/' + $location.path().split('/').pop()).then((resp) => {
     let src = resp.data.replace(/~2F/g, "/");
     $scope.clickedImg = src
@@ -49,35 +49,38 @@ imageboard.controller('photos', ($scope, $http, $location) => {
 imageboard.controller('comments', ($scope, $http, $stateParams, $location) => {
 
   $scope.limit = 4;
-  $scope.limitUpComments = function() {
+  $scope.limitUpComments = function () {
     $scope.limit += 4;
   };
 
-  $scope.submitComment = function() {
+  $scope.submitComment = function () {
     alert('Comment submit disabled in demo mode', 'ok');
   };
 
   function getComments() {
-    $http.get("https://litipsum.com/api/15/p/json").then(function(resp) {
+    $http.get("https://litipsum.com/api/15/p/json").then(function (resp) {
       let text = resp.data.text.join()
 
-      let allCommentObj= []
+      let allCommentObj = []
       let commentArr = []
-      
+
       let parArr = text.split('<p>', 10)
       for (const arr of parArr) {
         let newArr = arr.split("</p>").shift()
-       commentArr.push(newArr)
+        commentArr.push(newArr)
       }
+
+      for (let i = 1; i < commentArr.length; i++) {
+        let obj = {}
+
+        obj.name = commentArr[i].slice(8, 13).replace(/[^a-zA-Z0-9]/g, '').split().reverse().join() + commentArr[i].slice(20, 27).replace(/[^a-zA-Z0-9]/g, '')
+        obj.name = obj.name[0].toUpperCase() + obj.name.slice(1)
+        obj.description = commentArr[i < 3 ? 2 : 4].slice(0, 16).toUpperCase().replace(/[^a-zA-Z ]/g, "")
+        obj.comment = commentArr[i].slice(0, commentArr[i].indexOf('.')).replace(/[^a-zA-Z ]/g, "")
+        allCommentObj.push(obj)
+      }
+      console.log(allCommentObj);
       
-   for (let i = 1; i < commentArr.length; i++) {
-     let obj= {}
-     
-     obj.name = commentArr[i].slice(8, 13).replace(/[^a-zA-Z0-9]/g, '').split().reverse().join() + commentArr[i].slice(20, 27).replace(/[^a-zA-Z0-9]/g, '')
-      obj.name = obj.name[0].toUpperCase() + obj.name.slice(1)
-     obj.comment = commentArr[i].slice(0, commentArr[i].indexOf('.')).replace(/[^a-zA-Z ]/g, "")
-     allCommentObj.push(obj)
-   }
       $scope.comments = allCommentObj;
     });
   }
